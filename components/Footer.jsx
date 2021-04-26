8d6930ce7ec7f04f0ed89a4c929f0322610f2f53
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-26 10:21:37 +0200

    change the team name

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,16 @@
-export default function Footer(){
+export default function Footer() {
     return (
         <div className="text-center pt-2">
-            <a href="#" className="px-1">Home</a>
-            <a href="#" className="px-1">About</a>
-            <a href="#" className="px-1">Ads</a>
-            <div className="border-top pt-2">Developed by <a href="https://www.irumvanselme.cf" target="_blank">anselme</a></div>
+            <a href="#" className="px-1">
+                Home
+            </a>
+            <a href="#" className="px-1">
+                About
+            </a>
+            <a href="#" className="px-1">
+                Ads
+            </a>
+            <div className="border-top pt-2">Developed by codeIt </div>
         </div>
-    )
+    );
 }
